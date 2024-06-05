5444bb581f9acd46e67a164df9e1cf1066d8be10
sAmH365 <npower5377@naver.com>
2024-06-05 10:10:15 +0900

    user > email 컬럼 길이 수정

--- src/main/java/shop/mtcoding/bank/domain/user/User.java
+++ src/main/java/shop/mtcoding/bank/domain/user/User.java
@@ -27,7 +27,7 @@ public class User {
     @Column(nullable = false, length = 60)
     private String password;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 100)
     private String email;
 
     @Column(nullable = false, length = 20)
