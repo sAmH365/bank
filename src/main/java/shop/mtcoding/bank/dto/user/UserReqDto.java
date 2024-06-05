71479c904fefff84ba6ffdc8d24237e3b4cefd87
sAmH365 <npower5377@naver.com>
2024-06-05 10:10:45 +0900

    회원가입dto validation 적용

--- src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -1,7 +1,9 @@
 package shop.mtcoding.bank.dto.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
@@ -13,7 +15,7 @@ public class UserReqDto {
     @Getter
     @Setter
     public static class JoinReqDto {
-        @Pattern(regexp = "", message = "영문/숫자 2~20자로 작성해주세요.")
+        @Pattern(regexp = "^[0-9a-zA-Z]{2,20}$", message = "영문/숫자 2~20자로 작성해주세요.")
         @NotEmpty
         private String username;
 
@@ -22,8 +24,10 @@ public class UserReqDto {
         private String password;
 
         @NotEmpty
+        @Email
         private String email;
 
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z]{1,20}$", message = "영문/한글 1~20자로 작성해주세요.")
         @NotEmpty
         private String fullName;
 
